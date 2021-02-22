86ce5242de4c71ee379f3f80fd95a5159e4ade37
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-22 10:07:21 +0200

    made the classic guitar

---
+++ main.js
@@ -0,0 +1,8 @@
+class ClassicGuitar {
+  constructor(manufactureYear, brand, price) {
+    this.manufactureYear = manufactureYear;
+    this.brand = brand;
+    this.price = price;
+    this.used = false;
+    this.numberOfString = 6;
+  }
